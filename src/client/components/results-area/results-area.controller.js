450b7b8461e573af9827b61714b9bb486700865f
Steve Winkler <steve.winkler@ionep.com>
2015-06-20 16:10:39 +0000

    Added Client-side error handling for Grid

--- src/client/components/results-area/results-area.controller.js
+++ src/client/components/results-area/results-area.controller.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('defenderApp')
-  .controller('ResultsAreaCtrl', function ($scope) {
+  .controller('ResultsAreaCtrl', function ($scope, $http) {
     //this file pull data from server and populate bootstrap grid on client
+
+    //on error
+    $scope.errorHappenedResultsArea = false;
+
     $scope.recallResultsList = [];
-    $http.get('/api/things').success(function(recallResultsList) {
+    $http.get('/api/things')
+      .success(function (recallResultsList) {
       $scope.recallResultsList = recallResultsList;
+    })
+      .error(function () {
+        $scope.errorHappenedResultsArea = true;
+        console.log('got an error')
     });
   });
