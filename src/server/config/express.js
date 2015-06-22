6365f5a289f3e84e385c3725132d240b9c4fcdda
Steve Winkler <steve.winkler@ionep.com>
2015-06-22 16:58:50 -0400

    update logging format for continuous monitoring support

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -31,7 +31,7 @@ module.exports = function(app) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
+    app.use(morgan(':remote-addr :remote-user :method :url :date[iso] :status :res[content-length] - :response-time ms'));
   }
 
   if ('development' === env || 'test' === env) {
@@ -39,7 +39,7 @@ module.exports = function(app) {
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
-    app.use(morgan('dev'));
+    app.use(morgan(':remote-addr :remote-user :method :url :date[iso] :status :res[content-length] - :response-time ms'));
     app.use(errorHandler()); // Error handler - has to be last
   }
 };
