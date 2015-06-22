67d981c89fa59fd581648b851bdb00d8b00db508
Steve Winkler <steve.winkler@ionep.com>
2015-06-22 22:02:57 +0000

    Added tooltips to search buttons

--- src/client/components/refiner-area/refiner-area.controller.js
+++ src/client/components/refiner-area/refiner-area.controller.js
@@ -3,6 +3,10 @@
 var defender = defender || {};
 defender.searchTerm = "";
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
 angular.module('defenderApp')
   .controller('RefinerAreaCtrl', function ($scope) {
     //this file will hook in functionality to the refiner area such as FOOD.click, SearchBoxGo.Click
